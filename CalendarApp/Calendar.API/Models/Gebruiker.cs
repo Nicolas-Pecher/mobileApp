c647297891299ce5e2a581c1f1894f1bc7ccfd5f
CedricWillemsEHB <cedric.willems@student.ehb.be>
2019-03-04 15:14:31 +0100

    API Toegevoegd

---
+++ CalendarApp/Calendar.API/Models/Gebruiker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar.API.Models
+{
+    public class Gebruiker
+    {
+        public string gebruikerid { get; set; }
+        public string paswoord { get; set; }
+        public string email { get; set; }
+        public string rol { get; set; }
+        public string bedrijf { get; set; }
+        public List<TimeSheets> listTimesheet { get; set; }
+    }
+}
