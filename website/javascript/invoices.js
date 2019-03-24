6a253be4fa55367916288b40ee695f114ae398ea
xtripiansx <xtripiansx@gmail.com>
2019-03-24 20:07:47 +0100

    facturatie pagina auto maken (niet klaar)

--- website/javascript/invoices.js
+++ website/javascript/invoices.js
@@ -2,6 +2,7 @@ activePage('invoices');
 
 let bedrijven = [];
 let consultanten = [];
+let number = 0;
 
 //ajax get request om klanten(bedrijven) weer te geven in eerste drop down
 $.ajax({
@@ -10,9 +11,32 @@ $.ajax({
     success: function (data) {
 
         data.forEach(klant => {
+            number = number + 1;
             bedrijven.push(klant);
-            $('#klantFactuur').append(`<option class="selectKlant">${klant.Naam}</option>`);
+            $('#listKlanten').append(`<li class="list-group-item list-group-item-action" id="${klant.Naam}">${klant.Naam}</li>`);
+            //$('#klantFactuur').append(`<option id="selectKlant${number}">${klant.Naam}</option>`);
         });
+    }
+});
+
+//functie die een eventlistener toevoegt aan de option elements TODO ervoor zorgen dat wanneer user zich vergist de lijst niet groter wordt
+document.getElementById("listKlanten").addEventListener("click", function (e) {
+    if (e.target && e.target.nodeName == "LI") {
+        var geselecteerdeBedrijf = new Object();
+        for (let index = 0; index < bedrijven.length; index++) {
+            if (e.target.id == bedrijven[index].Naam) {
+                geselecteerdeBedrijf = bedrijven[index];
+            }
+        }
+        console.log(geselecteerdeBedrijf);
+        document.getElementById("straatnummer").append("straatnummer van " + e.target.id + " ");
+        document.getElementById("straatnaam").append("straatnaam van " + e.target.id + " ");
+        document.getElementById("postcode").append("postcode van " + e.target.id + " ");
+        document.getElementById("gemeente").append("gemeente van " + e.target.id + " ");
+        document.getElementById("land").append("land van " + e.target.id + " ");
+        document.getElementById("btwNummer").append(" "+ geselecteerdeBedrijf.BtwNummer);
+        rekenNr = "BE68539000000000";
+        document.getElementById("rekeningNummer").append(" " + rekenNr);//TODO met ajaxcall get de rekening nummer van bedrijf(klant)
     }
 });
 
@@ -21,13 +45,20 @@ $.ajax({
     type: "get",
     url: "http://mobileapp-planning-services.azurewebsites.net/api/Gebruiker",
     success: function (data) {
-        
+
         data.forEach(gebruiker => {
             if (gebruiker.Rol === "consultant") {
                 consultanten.push(gebruiker);
-                $('#consultantFactuur').append(`<option>${gebruiker.Naam}</option>`);
+                $('#listConsultanten').append(`<li class="list-group-item list-group-item-action" id="${gebruiker.Naam}">${gebruiker.Naam}</li>`);
             }
         });
+    }
+});
+
+//eventlistener toevoegen aan consultant optie
+document.getElementById("listConsultanten").addEventListener("click", function (e) {
+    if (e.target && e.target.nodeName == "LI") {
+        document.getElementById("naamConsultant").append(e.target.id);
     }
 });
 
@@ -54,25 +85,3 @@ document.getElementById("uitersteDatum").append(uitersteDatum);
 //rekeningnummer maken van ONS BEDRIJF
 rekNr = "BE68539007547034";
 document.getElementById("rekeningNummerOns").append(" " + rekNr);
-
-//door de klant te selecteren worden zijn gegevens automatisch ingevuld
-document.getElementsByClassName("selectKlant").addEventListener("click", function(){
-    
-    console.log(this);
-    var geselecteerdeBedrijf = new Object();
-    bedrijven.forEach(bedrijf => {
-        if (bedrijf.Naam == this ) {
-            geselecteerdeBedrijf.Naam = bedrijf.Naam;
-        }
-    });
-    document.getElementById("naamBedrijf").append(geselecteerdeBedrijf.Naam);
-    document.getElementById("straatnummer").append();
-    document.getElementById("straatnaam").append();
-    document.getElementById("postcode").append();
-    document.getElementById("gemeente").append();
-    document.getElementById("land").append();
-});
-
-//rekeningnummer maken van het klant(bedrijf)
-rekenNr = "BE685390000000000";
-document.getElementById("rekeningNummer").append(" " + rekenNr);//TODO met ajaxcall get de rekening nummer van bedrijf(klant)
