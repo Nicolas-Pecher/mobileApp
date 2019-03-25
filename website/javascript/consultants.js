73f518992107c86d3fd0187f1bedbcfae3518bd4
xtripiansx <xtripiansx@gmail.com>
2019-03-25 16:40:58 +0100

    verder api integreren

--- website/javascript/consultants.js
+++ website/javascript/consultants.js
@@ -2,6 +2,7 @@ activePage('consultants');
 
 let titles = ['Naam', 'Emailadres', 'Gewerkte uren', 'Overuren'];
 let data = [];
+let timesheetsConsultanten = [];
 
 //ajax get request om consultants weer te geven
 $.ajax({
@@ -18,3 +19,22 @@ $.ajax({
         displayList(titles, data, 'consultants');
     }
 });
+
+/*
+//ajax get request om timesheets van consultants weer te geven
+consultanten.forEach(element => {
+    console.log(element);
+});
+consultanten.forEach(consultant => {
+    $.ajax({
+        type: "get",
+        url: `http://mobileapp-planning-services.azurewebsites.net/api/ConsultantTimesheets/${consultant.GebruikerId}`,
+        success: function (response) {
+            
+            response.forEach(timesheetGebruiker => {
+                timesheetsConsultanten.push(timesheetGebruiker);
+            });
+        }
+    });
+});
+console.log(timesheetsConsultanten)*/
