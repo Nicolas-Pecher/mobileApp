9f8a3554c303c9fbd5dac9fb26a8d16abd302db3
Yasmine10 <yasminedewolf@hotmail.com>
2019-04-14 21:37:35 +0200

    styling profiel pagina lichtjes gewijzigd

--- website/pages/profile.php
+++ website/pages/profile.php
@@ -4,7 +4,6 @@
     if(!isset($_SESSION['gebruikerId'])) {
         header("Location: login.php");
     }
-
 ?>
 
 <body>
@@ -18,67 +17,84 @@
 
         <?php include './components/topNavigation.php'; ?>
 
-        <div class="container m-3 ml-3">
-            <p class="small pt-2"><i class="fas fa-home"></i>/ Profiel</p>
+        <div class="container my-4 mx-2">
+            <p class="small"><i class="fas fa-home"></i>/ &nbsp;Profiel</p>
         </div>
 
-        <div class="container m-3">
-            <h1>Profiel</h1>
+        <div class="container-fluid mx-2">
 
-            <div class="mt-4 pr-4">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <h6>Gegevens</h6>
-                        <table class="table table-sm table-borderless">
-                            <tbody>
-                                <tr>
-                                    <th>Naam</th>
-                                    <td><?php echo $_SESSION['naam']; ?></td>
-                                </tr>
-                                <tr>
-                                    <th>Emailadres</th>
-                                    <td><?php echo $_SESSION['email']; ?></td>
-                                </tr>
-                                <tr>
-                                    <th>Werkgever</th>
-                                    <td id="bedrijfNaamProfile"></td>
-                                    <input type="text" id="bedrijfIdProfile" value="<?php echo $_SESSION['bedrijfId']; ?>" style="display:none;" >
-                                </tr>
-                                <tr>
-                                    <th>Rol</th>
-                                    <td><?php echo $_SESSION['gebruikerRol']; ?></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <form action="" class="col-sm-4">
-                        <div class="form-group">
-                            <label for="">Wijzig wachtwoord</label>
-                            <input type="password" placeholder="Huidig wachtwoord" class="form-control form-control-sm mb-2 mt-2 col-sx-4">
-                            <input type="password" placeholder="Nieuw wachtwoord" class="form-control form-control-sm">
+            <h1 class="mb-4">Profiel</h1>
+
+            <div class="row">
+                <div class="col-sm-6">
+
+                    <div class="card mb-4">
+                        <div class="card-header" style="">
+                            <h5 class="">Persoonlijke gegevens</h5>
                         </div>
-                        <button type="submit" class="btn btn-sm" id="changePasswordBtn">Wachtwoord wijzigen</button>
-                    </form>
-                </div>
-                <div class="row">
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group">
+                                    <label for="name">Naam:</label><br>
+                                    <input type="text" name="name" class="form-control form-control-sm" value="<?php echo $_SESSION['naam']; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Emailadres:</label>
+                                    <input type="email" name="email" class="form-control form-control-sm" value="<?php echo $_SESSION['email']; ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="boss">Werkgever:</label>
+                                    <input type="text" name="boss" id="bedrijfNaamProfile" class="form-control form-control-sm">
+                                    <input type="text" id="bedrijfIdProfile" value="<?php echo $_SESSION['bedrijfId']; ?>" style="display: none;" >
+                                </div>
+                                <div class="form-group">
+                                    <label for="roll">Rol:</label>
+                                    <input type="text" name="roll" class="form-control form-control-sm" value="<?php echo $_SESSION['gebruikerRol']; ?>">
+                                </div>
+                                <button type="submit" class="btn btn-sm pl-4 pr-4 mt-3" id="changePasswordBtn">Wijzigen</button>
+                            </form>
+                        </div>
+
+                    </div>
 
                 </div>
+
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-header" style="">
+                            <h5 class="">Wachtwoord wijzigen</h5>
+                        </div>
+                        <div class="card-body">
+                            <form action="../controllers/changePassword.php" method="post" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label for="currentPassword">Nieuw wachtwoord:</label>
+                                    <input type="password" name="Password" class="form-control form-control-sm">
+                                </div>
+                                <div class="form-group">
+                                    <label for="newPassword">Bevestig wachtwoord:</label>
+                                    <input type="password" name="newPassword" class="form-control form-control-sm">
+                                </div>
+                                <button type="submit" class="btn btn-sm" id="changePasswordBtn">Wachtwoord wijzigen</button>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
 
-        </div>
+            <a href="#topPage" class="btn" id="pageButton"><i class="fas fa-caret-up"></i></a>
 
-        <a href="#topPage" class="btn" id="pageButton"><i class="fas fa-caret-up"></i></a>
+        </div>
+        <!-- /#page-content-wrapper -->
 
     </div>
-    <!-- /#page-content-wrapper -->
+    <!-- /#wrapper -->
 
-</div>
-<!-- /#wrapper -->
+    <?php include './components/footer.php'; ?>
 
-<?php include './components/footer.php'; ?>
-
-<script src="../javascript/showActivePage.js"></script>
-<script src="../javascript/profile.js"></script>
+    <script src="../javascript/showActivePage.js"></script>
+    <script src="../javascript/profile.js"></script>
 
 </body>
 
