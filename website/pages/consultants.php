4385a4ce4bae50c42a6106791a0f075100e50ccb
Yasmine10 <yasminedewolf@hotmail.com>
2019-04-15 23:50:11 +0200

    kleine aanpassingen

--- website/pages/consultants.php
+++ website/pages/consultants.php
@@ -17,29 +17,37 @@
 
         <?php include './components/topNavigation.php'; ?>
 
-        <div class="container my-4 mx-2">
-            <p class="small"><i class="fas fa-home"></i>/ &nbsp;Consultants</p>
-        </div>
+        <div id="content">
 
-        <div class="container-fluid mx-2">
-            <h1 class="mb-4">Consultants</h1>
+            <div class="container my-4 mx-2">
+                <p class="small"><i class="fas fa-home"></i>/ &nbsp;Consultants</p>
+            </div>
 
-            <div class="mt-3 d-flex">
-                <div class="">
-                    <a href="addConsultant.php" class="btn timesheetsBtn mr-2"><i class="fas fa-plus"></i> Toevoegen</a>
+            <div class="container-fluid mx-2">
+                <h1 class="mb-4">Consultants</h1>
+
+                <div class="mt-3 d-flex">
+                    <div class="">
+                        <a href="addConsultant.php" class="btn timesheetsBtn mr-2"><i class="fas fa-plus"></i> Toevoegen</a>
+                    </div>
+                </div>
+
+                <div class="my-4 tabledisplay">
+                    <!--The consultants table is displayed here-->
+
                 </div>
             </div>
 
-            <div class="mt-4 mb-4 tabledisplay">
-                <option id="bedrijfIdVoorConsultant" value="<?php echo $_SESSION['bedrijfId']; ?>" style="display:none;">
-               <!--The consultants table is displayed here-->
-               
-            </div>
         </div>
+
+
+
 
         <a href="#topPage" class="btn" id="pageButton"><i class="fas fa-caret-up"></i></a>
 
         <input type="text" id="bedrijfIdConsultants" value="<?php echo $_SESSION['bedrijfId']; ?>" style="display:none;" >
+
+        <footer class="border-top small text-center"><p class="pt-2">&copy; Copyright 2019, Project Mobile Apps groep 2</p></footer>
 
     </div>
     <!-- /#page-content-wrapper -->
