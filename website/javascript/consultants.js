d2a1e2ee4ee019e5898d1feae15feb811dca6a3a
xtripiansx <xtripiansx@gmail.com>
2019-05-03 21:47:54 +0200

    invoice pagina layout veranderen + reminder maken

--- website/javascript/consultants.js
+++ website/javascript/consultants.js
@@ -21,6 +21,40 @@ $(document).ready(function () {
             });
 
             displayList(titles, data, 'consultants');
+
+            function isHetEindeVanMaand(vandaag, eindeMaand) {
+                if (vandaag === eindeMaand) {
+                    console.log("true");
+                } else {
+                    data.forEach(consultant => {
+                        console.log(consultant);
+                        var addresses = consultant.Email;
+                        var body = "Heeft u uw timesheets ingevuld?"
+                        var subject = "Timesheets"
+                        var href = "mailto:" + addresses + "?"
+                            + "subject=" + subject + "&"
+                            + "body=" + body;
+                        var wndMail;
+                        wndMail = window.open(href, "scrollbars=yes,resizable=yes,width=10,height=10");
+                    });
+                }
+            }
+        
+            var nu = new Date();
+            eindeMaand = new Date(nu.getFullYear(), nu.getMonth() + 1, 0);
+        
+            var dd = String(nu.getDate()).padStart(2, '0');
+            var mm = String(nu.getMonth() + 1).padStart(2, '0');
+        
+            var vandaag = dd + "/" + mm;
+        
+            var dag = String(eindeMaand.getDate()).padStart(2, '0');
+            var maand = String(eindeMaand.getMonth() + 1).padStart(2, '0');
+        
+            var einde = dag + "/" + maand;
+        
+            isHetEindeVanMaand(vandaag, einde);
+
         }
     });
 
