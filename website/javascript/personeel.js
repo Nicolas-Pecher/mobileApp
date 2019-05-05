278fc69dfd1bd04ef441102334965aa84d63d3a9
Nicolas Pecher <nicolas.pecher@student.ehb.be>
2019-05-05 13:13:39 +0200

    begin personeel management

--- website/javascript/personeel.js
+++ website/javascript/personeel.js
@@ -1,28 +1,20 @@
 $(document).ready(function () {
 
-    activePage('consultants');
+    activePage('managePersoneel');
 
-    let bedrijfId = $('#bedrijfIdConsultants').val();
+    let bedrijfId = $('#bedrijfId').val();
     console.log("BedrijfId = " + bedrijfId);
 
     let titles = ['#', 'Naam', 'Emailadres'];
-    let data = [];
 
-    //ajax get request om consultants weer te geven
+    //ajax get request om Personeel weer te geven
     $.ajax({
         type: 'GET',
         url: 'http://mobileapp-planning-services.azurewebsites.net/api/OverzichtConsultants/' + bedrijfId,
         success: function (response) {
 
-            //console.log(response);
-
-            response.forEach(consultant => {
-                data.push(consultant);
-            });
-
+            data = response;
             displayList(titles, data, 'consultants');
-
-            
 
         }
     });
