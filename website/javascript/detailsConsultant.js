e1fa5b1f2b797ca5b684e1c8be756b3cbd56db62
DE WOLF Yasmine <yasminedewolf@hotmail.com>
2019-05-10 03:34:51 +0200

    functionaliteit wijzigen consultant gegevens geschreven (geeft nog een error)

--- website/javascript/detailsConsultant.js
+++ website/javascript/detailsConsultant.js
@@ -1,50 +1,109 @@
 $(document).ready(function () {
 
-    console.log("in detailsConsultant.js");
-
     activePage('consultants');
 
-    //let consultantId = $('#consultantId').val();
-
-    //console.log(con)
-
     let consultantId = location.search.substring(1);
-    console.log(consultantId);
+    let bedrijfId = $("#bedrijfIdValue").val();
 
     $.ajax({
         type: 'GET',
         url: 'https://mobileapp-planning-services.azurewebsites.net/api/Gebruiker/' + consultantId,
         success: function (data) {
             console.log(data);
-
-            let naam = $('.consultantNaam');
-
-            naam.attr('value', data.GebruikerNaam).text(data.GebruikerNaam);
-
+            $("#naam").attr("value", data.GebruikerNaam).text(data.GebruikerNaam);
+            $("#naamConsultant").attr("value", data.GebruikerNaam).text(data.GebruikerNaam);
+            $("#emailConsultant").attr("value", data.Email).text(data.Email);
+            $("#loonConsultant").attr("value", data.LoonPerUur).text(data.LoonPerUur);
+            $("[name='wachtwoordConsultant']").attr("value", data.Wachtwoord);
+            password = data.Wachtwoord;
         }
+    });
+
+    $("#wijzigConsultantForm").submit(function (e) {
+
+        e.preventDefault();
+
+        //validate
+
+        // let naam = $("[name='naamConsultant']").val();
+        // let email = $("[name='emailConsultant']").val();
+        // let loon = $("[name='loonConsultant']").val();
+        // let wachtwoord = $("[name='wachtwoordConsultant']").val();
+        //
+        // let dataJSON = {
+        //     GebruikerNaam: naam,
+        //     Email: email,
+        //     Wachtwoord: wachtwoord,
+        //     Rol: "consultant",
+        //     BedrijfId: bedrijfId,
+        //     LoonPerUur: loon
+        // };
+        //
+        // $.ajax({
+        //     type: 'PUT',
+        //     url: 'https://mobileapp-planning-services.azurewebsites.net/api/Gebruiker' + consultantId,
+        //     data: JSON.stringify(dataJSON),
+        //     contentType: 'application/json; charset=utf-8',
+        //     dataType: 'json',
+        //     success: function (data) {
+        //         console.log(data);
+        //         //document.getElementById("formTimeEntry").reset();
+        //         //location.assign('./timesheets.php');
+        //     },
+        //     error: function (data) {
+        //         console.log(data);
+        //     }
+        // });
+
+        let dataJSON = {
+            GebruikerId: 9,
+            ProjectId: 4,
+            Datum: datum,
+            Beginuur: beginuur,
+            Einduur: einduur,
+            Opmerking: opmerking,
+            Overuur: overuren
+        };
+
+        $.ajax({
+            type: 'PUT',
+            url: 'https://mobileapp-planning-services.azurewebsites.net/api/Timesheet/' + timesheetId,
+            data: JSON.stringify(dataJSON),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                console.log("Updated Successfully : " + data);
+                alert("Timesheet succesvol gewijzigd!");
+            },
+            error: function (data) {
+                //console.log(data);
+            }
+        });
+
     });
 
     $.ajax({
         type:'GET',
         url: 'https://mobileapp-planning-services.azurewebsites.net/api/ProjectVanGebruiker/'+ consultantId,
         success: function (data) {
-            console.log(data);
+            //console.log(data);
 
             let body = $('#huidigeProjecten');
 
-
-
             if(data.length != 0) {
+
+                let number = 1;
+
                 data.forEach(project => {
                     let row = $('<tr></tr>');
+                    row.append($('<td></td>').text(number));
                     row.append($('<td></td>').attr('value', project.ProjectNaam).text(project.ProjectNaam));
                     body.append(row);
+                    number++;
                 });
             } else {
                 body.append($('<td class="p-4"></td>').attr('value', "Geen data").text("Geen data"));
             }
-
-
         }
     });
 
@@ -68,8 +127,9 @@ $(document).ready(function () {
                 div.append($('<p class="p-4"></p>').attr('value', "Geen data").text("Geen data"));
             }
 
-
         }
     });
+
+
 
 });
--- website/pages/detailsConsultant.php
+++ website/pages/detailsConsultant.php
@@ -23,34 +23,73 @@
 
         <div class="container-fluid mx-2">
 
-            <h1 class="consultantNaam"></h1>
+            <h1 id="naam"></h1>
             <p>Consultant</p>
             <hr class="mr-4 mb-4">
 
             <div class="row">
-                <div class="col-sm-3 mb-4">
+                <div class="col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="">Huidige projecten</h5>
+                        </div>
+                        <div class="card-body">
+                            <form id="modifyConsultantForm">
+                                <div class="form-group">
+                                    <label for="naamConsultant">Voornaam en achternaam:</label>
+                                    <input type="text" name="naamConsultant" id="naamConsultant" class="form-control form-control-sm"/>
+                                    <span class="text-danger small" id="naamConsultantError"></span>
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Emailadres:</label>
+                                    <input type="email" name="emailConsultant" id="emailConsultant" class="form-control form-control-sm"/>
+                                    <span class="text-danger small" id="emailConsultantError"></span>
+                                </div>
+                                <div class="form-group">
+                                    <label for="loon">Loon per uur:</label>
+                                    <input type="text" name="loonConsultant" id="loonConsultant" class="form-control form-control-sm">
+                                    <span class="text-danger small" id="loonConsultantError"></span>
+                                </div>
+                                <input type="password" name="wachtwoordConsultant" style="display:none;" >
+                                <input type="text" name="bedrijfId" id="bedrijfIdValue" value="<?php echo $_SESSION['bedrijfId']; ?>" style="display:none;" >
+                                <br>
+                                <button type="submit" name="submit" class="btn btn-sm px-4 mb-2" id="modifyConsultantBtn">Wijzigen</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mb-4">
                     <div class="card" style="min-height: 50vh;">
                         <div class="card-header">
                             <h5 class="">Huidige projecten</h5>
                         </div>
-                        <table class="table table-bordered thead-light">
+                        <table class="table table-striped border">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Naam</th>
+                            </tr>
+                            </thead>
                             <tbody id="huidigeProjecten">
 
                             </tbody>
                         </table>
                     </div>
-<!--                        <form action="" class="d-flex mt-2">-->
-<!--                            <div class="form-group">-->
-<!--                                <select class="form-control form-control-sm" id="projectConsultant">-->
-<!--                                    <option>Kies Project</option>-->
-<!--                                    <option>Integration project</option>-->
-<!--                                    <option>Software project II</option>-->
-<!--                                </select>-->
-<!--                            </div>-->
-<!--                            <a href="" class="btn btn-sm mb-4 ml-2" id="extraEntryBtn"><i class="fas fa-plus"></i></a>-->
-<!--                        </form>-->
+                    <!--                        <form action="" class="d-flex mt-2">-->
+                    <!--                            <div class="form-group">-->
+                    <!--                                <select class="form-control form-control-sm" id="projectConsultant">-->
+                    <!--                                    <option>Kies Project</option>-->
+                    <!--                                    <option>Integration project</option>-->
+                    <!--                                    <option>Software project II</option>-->
+                    <!--                                </select>-->
+                    <!--                            </div>-->
+                    <!--                            <a href="" class="btn btn-sm mb-4 ml-2" id="extraEntryBtn"><i class="fas fa-plus"></i></a>-->
+                    <!--                        </form>-->
                 </div>
-                <div class="col-sm-9">
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12">
                     <div class="card mb-4" style="min-height: 50vh;">
                         <div class="card-header">
                             <h5 class="">Timesheets</h5>
@@ -61,8 +100,6 @@
                     </div>
                 </div>
             </div>
-
-
 
             <a href="#topPage" class="btn" id="pageButton"><i class="fas fa-caret-up"></i></a>
 
@@ -80,3 +117,4 @@
     <script src="../javascript/deleteTimesheet.js"></script>
 
 </body>
+</html>
